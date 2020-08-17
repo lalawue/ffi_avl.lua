b28b25b5f3362685a9372324d8f9ccbcb69a1b2f
lalawue <suchaaa@gmail.com>
2020-08-17 23:28:56 +0800

    fix node remove, update text.lua

--- test.lua
+++ test.lua
@@ -7,6 +7,16 @@ local tree =
     end
 )
 
+local function _printTree(tag, tree)
+    io.write(tag)
+    local v, h = tree:first()
+    repeat
+        io.write(string.format("%d (h:%d), ", v, h))
+        v, h = tree:next(v)
+    until v == nil
+    print("")
+end
+
 io.write("inserts: ")
 -- insert '9, 7, 5, 3, 1'
 for i = 9, 1, -2 do
@@ -23,28 +33,7 @@ tree:insert(4)
 tree:insert(8)
 
 -- print '1, 3, 4, 5, 7, 8, 9'
-io.write("values : ")
-local n = tree:first()
-repeat
-    io.write(n)
-    n = tree:next(n)
-    if n then
-        io.write(", ")
-    end
-until n == nil
-print("")
-
--- print node height
-io.write("heights: ")
-local n, h = tree:first()
-repeat
-    io.write(h)
-    n, h = tree:next(n)
-    if n then
-        io.write(", ")
-    end
-until n == nil
-print("")
+_printTree("values : ", tree)
 
 -- prev
 io.write(" prevs : ")
@@ -58,11 +47,13 @@ repeat
 until n == nil
 print("")
 
-io.write("remove 9, 7, 4: \t")
-tree:remove(9)
+tree:remove(5)
+_printTree("remove 5: ", tree)
 tree:remove(7)
-tree:remove(4)
+_printTree("remove 7: ", tree)
+tree:remove(3)
 
+io.write("remove 3 walk: ")
 for value, height in tree:walk() do
     io.write(string.format("%d (h:%d), ", value, height))
 end
